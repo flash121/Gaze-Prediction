4db4b03e235331de2e2e6454d5597d1b574df8ec
flash121 <gaoyfeng2010@hotmail.com>
2013-12-15 15:04:28 -0800

    Separate Data Success

--- src/ReadData.py
+++ src/ReadData.py
@@ -3,14 +3,28 @@ Created on 2013-12-15
 
 @author: yfeng
 '''
-
+import numpy as np
+from Gaze import Gaze
 class ReadData(object):
     '''
     Reading raw data.txt content, store into Gaze Class List
     '''
 
-    def __init__(self,name='data.csv'):
+    def __init__(self,name='data1.txt'):
         '''
         reading process
         '''
+        f=open(name,'r')
+        data=[line.split() for line in f]
+        #dive into each sample
+        l=len(data)
+        termin=[x-1 for x in range(1,l) if data[x][7]!=data[x-1][7]]
+        ndata=np.array(data) #using np array to doing vector operation
+        del data #release data
+        termin.insert(0, -1)
+        termin.append(l-1)
+        batch=[ndata[termin[ind]+1:termin[ind+1],:] for ind in range(0,len(termin)-1)]
+        self.gaze=[Gaze(b) for b in batch]
         
+        
+u=ReadData()
