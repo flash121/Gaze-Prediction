35f3d2979519039724c4a383745ee18df53d2584
flash121 <gaoyfeng2010@hotmail.com>
2013-12-17 20:35:24 -0800

    Gaze model update [ISSUSE 1-3] Other related information need update

--- src/Gaze.py
+++ src/Gaze.py
@@ -7,15 +7,18 @@ import numpy as np
 import scipy.stats as stat
 class Gaze(object):
     '''
+    Gaze Class:
+    @note: this class [Gaze] for basic data item operations
+    @note: now provide: [stack]
+    @note: store-type:  [trainAR], [trainSVR], [raw data] 
+    @note: other annotation only available before stack
     GazeDataStore Class
-    gaze x-y
-    mouse x-y
-    user
-    click status 
-    notes
+    @version: 0.01
+    ------------------------------------------------
+    @author:  yfeng
     '''
 
-    def __init__(self,data):
+    def __init__(self,data,order=10):
         '''
         store data into class
         '''
@@ -32,6 +35,8 @@ class Gaze(object):
         del data
         self.corr=self.correction()
         self.corr[np.isnan(self.corr)]=0
+        self.ConvertARtrain(self.mouse, order)
+        self.ConvertSVRtrain(self.mouse, self.gaze, order)
         return
     
         
@@ -39,4 +44,58 @@ class Gaze(object):
         temp=np.hstack((self.gaze,self.mouse)) 
         correctV=np.array([stat.pearsonr(temp[:,i], temp[:,j])[0] for i in range(0,4) for j in range(0,4) if(i>j)],dtype='float64')
         return correctV
+    def ConvertARtrain(self,label,order):
+        '''
+        AR train version
+        @param  label: label <-> numpy
+        @param order: order for learning
+        '''
+        labelX=label[:,0]
+        labelY=label[:,1]
+        l=label.shape[0]
+        XX=labelX[0:order]
+        XY=labelX[order]
+        YX=labelY[0:order]
+        YY=labelY[order]
+        for i in range(1,l-order-1):
+            XX=np.vstack((XX,labelX[i:i+order]))
+            XY=np.vstack((XY,labelX[i+order]))
+            YX=np.vstack((YX,labelY[i:i+order]))
+            YY=np.vstack((YY,labelY[i+order]))
+        self.ARX=(XX,YX)
+        self.ARY=(XY,YY)
+    def ConvertSVRtrain(self,label,target,order):
+        '''
+        SVR train version
+        @param label: label <> numpy, for mouse
+        @param order: order for learning
+        @param target: gaze position  
+        '''
         
+        labelX=label[:,0]
+        labelY=label[:,1]
+        targetX=target[:,0]
+        targetY=target[:,1]
+        l=label.shape[0]
+        XX=labelX[0:order]
+        XY=targetX[order-1]
+        YX=labelY[0:order]
+        YY=targetY[order-1]
+        for i in range(1,l-order-1):
+            XX=np.vstack((XX,labelX[i:i+order]))
+            XY=np.vstack((XY,targetX[i+order-1]))
+            YX=np.vstack((YX,labelY[i:i+order]))
+            YY=np.vstack((YY,targetY[i+order-1]))
+        self.SVRX=(XX,YX)
+        self.SVRY=(XY,YY)            
+    def stack(self,stack):
+        '''
+        gaze.stack(gaze2)
+        stack two object
+        Only combined SVRX/Y & ARX/Y will combined, other remains, 
+        for training process
+        '''
+        self.ARX=(np.vstack((self.ARX[0],stack.ARX[0])),np.vstack((self.ARX[1],stack.ARX[1])))
+        self.ARY=(np.vstack((self.ARY[0],stack.ARY[0])),np.vstack((self.ARY[1],stack.ARY[1])))
+        self.SVRX=(np.vstack((self.SVRX[0],stack.SVRX[0])),np.vstack((self.SVRX[1],stack.SVRX[1])))
+        self.SVRY=(np.vstack((self.SVRY[0],stack.SVRY[0])),np.vstack((self.SVRY[1],stack.SVRY[1])))
