8596e9756f97cd6e5aa2cc0cc1466f085e32dccd
flash121 <gaoyfeng2010@hotmail.com>
2013-12-18 02:55:01 -0800

    Clean unnecessary part/ initial predict func

--- src/Cluster.py
+++ src/Cluster.py
@@ -14,10 +14,9 @@ class GazeFit(object):
     @param SVR parameter:   
     '''
 
-
-    def __init__(self,model=None,data=None,order=(1,0),C=1.0,eps=0.2,nclus=10):
+    def __init__(self,model=None,data=None,order=10,C=1.0,eps=0.2,nclus=10):
         '''
-        Store cluster information
+        @note: Store cluster information
         '''
         self.kmode=model
         self.order=order
@@ -28,13 +27,15 @@ class GazeFit(object):
         self.svrparam=[]
         self.armaparam=[]
     def fit(self):
+        '''
+        @note: Overall fit process
+        '''
         label=np.array(self.kmode.labels_)
         i=0
         for l in range(0,self.nclus):
             index=label==l
             labgaze=self.gaze[index]
-            ga=labgaze[0]
-            
+            ga=labgaze[0]           
             for gaze in labgaze:
                 if(i==0):
                     i+=1
@@ -46,16 +47,34 @@ class GazeFit(object):
             self.AR(ga)
    
     def SVR(self,ga):
+        '''
+        @note: SVR train processing
+        '''
         self.svrx.fit(ga.SVRX[0], ga.SVRX[1])
         self.svry.fit(ga.SVRY[0], ga.SVRY[1])
         self.svrparam.append((self.svrx,self.svry))
     def AR(self,ga):
+        '''
+        @note: AR train processing
+        '''
         self.svrx.fit(ga.ARX[0], ga.ARX[1])
         self.svry.fit(ga.ARY[0], ga.ARY[1])
         self.armaparam.append((self.svrx,self.svry))
+    def predict(self,x):
+        '''
+        @note: predict function
+        @note: basic idea;
+               1. classify cluster by AR-paramters, choose best number 
+               2. SVR model regression -> gaze position (for x,y)
+        @param x: 1xorder+1 time series
+        '''
+        pass
+        
     '''
+    @note: older part/plan to delete
     def ARMA(self,data):
         arm=model.ARMA(data)
         result=arm.fit(order=self.order)
         return (result.arparams,result.maparams)
     '''
+    
