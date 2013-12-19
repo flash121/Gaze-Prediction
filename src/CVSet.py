71ef7dc9ca46aabd1d2e7b3cbf4b327e1e6b3770
flash121 <gaoyfeng2010@hotmail.com>
2013-12-18 20:45:42 -0800

    initial basic func

--- src/CVSet.py
+++ src/CVSet.py
@@ -3,6 +3,9 @@ Created on 2013-12-18
 
 @author: yfeng
 '''
+import Gaze
+import GazeFit
+from sklearn import cross_validation
 
 class CVTest(object):
     '''
@@ -12,7 +15,7 @@ class CVTest(object):
     '''
 
 
-    def __init__(self,data=None,mode='SVR',per=0.8):
+    def __init__(self,data=None,mode='SVR',per=0.8,N=10):
         '''
         Constructor
         @param data: 1xn gaze objects
@@ -22,3 +25,33 @@ class CVTest(object):
         self.data=data
         self.mode=mode
         self.per=per
+        self.N=N
+        self.flag=True
+        self.scoring=[]
+    def __iter__(self):
+        '''
+        @note: each iter -> one time cross_validation
+        @note: usage:
+                        [result]=[score for [object] in [CVTest]]                
+        '''
+        if self.flag:
+            pass
+        else:
+            pass
+    def __str__(self):
+        '''
+        @note: show status
+        '''
+        pass
+    def __getitem__(self,k):
+        try:
+            return self.scoring[k]
+        except:
+            return None    
+    def CVscoring(self):
+        '''
+        @note: main CV processing
+        '''
+        pass
+    def clean(self):
+        self.flag=True
